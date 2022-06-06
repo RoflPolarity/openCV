694f381966d56da9b4b1af6987dde342731573ff
RoflPolarity <ignis604@gmail.com>
2022-06-06 23:15:39 +0300

    working videoHandler

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,7 @@
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.CvType;
@@ -5,18 +9,14 @@ import org.opencv.core.Scalar;
 
 
 public class main {
-    static {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to OpenCV " + Core.VERSION);
-        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-        System.out.println("OpenCV Mat: " + m);
-        Mat mr1 = m.row(1);
-        mr1.setTo(new Scalar(1));
-        Mat mc5 = m.col(5);
-        mc5.setTo(new Scalar(5));
-        System.out.println("OpenCV Mat data:\n" + m.dump());
+    public static void main(String[] args) throws FrameGrabber.Exception {
+        OpenCVFrameGrabber CVGrab = new OpenCVFrameGrabber(0);
+        CVGrab.start();
+        Frame frame = CVGrab.grab();
+        CanvasFrame  CF = new CanvasFrame("");
+        CF.setCanvasSize(frame.imageWidth,frame.imageHeight);
+        while (CF.isVisible()&&(frame = CVGrab.grab())!=null){
+            CF.showImage(frame);
+        }
     }
 }
